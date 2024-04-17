f80e26a778f74e6797b395f946d32c45f646f372
Francisco Sierra Ares <franciscosa98@educastur.es>
2024-04-17 06:47:47 +0200

    Añadidos comentarios al código. Versión 1

--- Program.cs
+++ Program.cs
@@ -10,19 +10,29 @@ namespace Practica_Dados
     {
         static void Main(string[] args)
         {
+            /**
+            * Se crean dos variables que almacenan los valores de los dados.
+            */
             int valorDado1 = 0;
             int valorDado2 = 0;
-            Random rand = new Random();
-            int contador = 0;
+            Random rand = new Random(); // Random que se utiliza para generar los valores.
+            int contador = 0; // Contador para las tiradas
 
-            while(valorDado1 != 6 || valorDado2 != 6)
+            /**
+            * Se utiliza un bucle para realizar tiradas hasta que ambos valores sean 6.
+            */
+            while (valorDado1 != 6 || valorDado2 != 6)
             {
+                /**
+                 * Se genera el valor entre los valores especificados sin incluir el más alto.
+                 */
                 valorDado1 = rand.Next(1,7);
                 valorDado2 = rand.Next(1,7);
                 Console.WriteLine("Dado 1 - Dado 2");
                 Console.WriteLine("  " + valorDado1 + "        " + valorDado2);
                 contador++;
             }
+            // Al terminar se imprimen las tiradas.
             Console.WriteLine("Número de tiradas: " + contador);
         }
     }
